3b2550452fda5e26cc6b5ae811a0ff0fa3cca43c
AmrMohamadSalah <amrmohamadsalah0@gmail.com>
2021-11-29 12:35:34 +0200

    ......

--- table.py
+++ table.py
@@ -1,38 +1,41 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget, QTableWidgetItem, QVBoxLayout, \
-    QHeaderView
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
-import sys
-import functions
-
-data, rows = functions.bisection('x**4-2*x**3-4*x**2-4*x+4', 2, 0, 20, 0.01)
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
 
 
-class TableView(QTableWidget):
-    def __init__(self, data, *args):
-        QTableWidget.__init__(self, *args)
+class Ui_outputWindow(object):
+    def setupUi(self, outputWindow, data, rows, columns):
+        outputWindow.setObjectName("outputWindow")
+        outputWindow.resize(800, 600)
+
+        self.centralwidget = QtWidgets.QWidget(outputWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(95, 111, 570, 300))
+        self.tableWidget.setObjectName("tableWidget")
         self.data = data
-        self.setData()
-        self.resizeColumnsToContents()
-        self.resizeRowsToContents()
+        self.tableWidget.setRowCount(rows)
+        self.tableWidget.setColumnCount(columns)
+        self.setDataItems(data)
+        outputWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(outputWindow)
+        QtCore.QMetaObject.connectSlotsByName(outputWindow)
 
-    def setData(self):
-        horHeaders = []
+    def retranslateUi(self, outputWindow):
+        _translate = QtCore.QCoreApplication.translate
+        outputWindow.setWindowTitle(_translate("outputWindow", "Output"))
+
+    def setDataItems(self, data):
+        headers = []
         for n, key in enumerate(self.data.keys()):
-            horHeaders.append(key)
-            for m, item in enumerate(self.data[key]):
-                newitem = QTableWidgetItem(str(item))
-                self.setItem(m, n, newitem)
-        self.setHorizontalHeaderLabels(horHeaders)
+            headers.append(key)
+            for m, value in enumerate(self.data[key]):
+                newitem = QTableWidgetItem(str(value))
+                self.tableWidget.setItem(m, n, newitem)
+        self.tableWidget.setHorizontalHeaderLabels(headers)
+        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.resizeColumnsToContents()
+        self.tableWidget.resizeRowsToContents()
 
 
-def main(args):
-    app = QApplication(args)
-    table = TableView(data, rows, 6)
-    table.show()
-    print(data)
-    sys.exit(app.exec_())
 
-
-if __name__ == "__main__":
-    main(sys.argv)
